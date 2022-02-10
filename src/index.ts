60afee760709b6e7d57b45ca291662ff3cdd707c
Omar <elmalakomar@gmail.com>
2022-02-10 20:45:10 +0100

    made tail recursion. Hopefully correct

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,28 @@
-const example = {
-  a: '1',
-  b: '2',
-  c: {
-    d: '4',
-    e: '5',
-  },
-}
-
-const toMask = [
-  { field: 'b', type: 'default' },
-  {
-    field: 'c',
-    type: 'default',
-    data: [{ field: 'd', type: 'default' }],
-  },
-]
-
 const isObject = (data: any): Boolean => {
   return typeof data === 'object' && !Array.isArray(data) && data !== null
 }
 
 const redact = (obj: Record<string, any>, toRedact: any) => {
-  const newObj = Object.assign({}, obj)
-  const availableKeys = Object.keys(newObj)
-  toRedact.forEach((bo: any) => {
-    const fieldToManipulate = bo.field
-    if (availableKeys.includes(fieldToManipulate)) {
-      if (isObject(newObj[bo.field]) && bo.data) {
-        newObj[bo.field] = redact(newObj[bo.field], bo.data)
-        return
-      }
-      return (newObj[bo.field] = '[REDACT]')
-    }
-  })
-  return newObj
-}
+  const result = JSON.parse(JSON.stringify(obj))
 
-console.log(example)
-const x = redact(example, toMask)
-console.log(x)
-console.log(example)
+  const internalRedact = (
+    obj: Record<string, any>,
+    toRedact: any,
+    result: Record<string, any>
+  ) => {
+    const availableKeys = Object.keys(result)
+    toRedact.forEach((bo: any) => {
+      const fieldToManipulate = bo.field
+      if (availableKeys.includes(fieldToManipulate)) {
+        if (isObject(result[bo.field]) && bo.data) {
+          internalRedact(obj[bo.field], bo.data, result[bo.field])
+          return
+        }
+        return (result[bo.field] = '[REDACT]')
+      }
+    })
+    return
+  }
+  internalRedact(obj, toRedact, result)
+  return result
+}
