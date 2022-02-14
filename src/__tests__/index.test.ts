081af8c47ecf1a5ecc9f353a2b389767058ac8d3
omar.elmalak <omar.elmalak@mia-platform.eu>
2022-02-14 08:33:59 +0100

    refactor

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,21 @@
-describe('empty test', () => {
-  it('expect to be true', () => {
-    expect(true).toBeTruthy()
+const { redact } = require('../index')
+describe('redact', () => {
+  describe('simple objects', () => {
+    const obj = { a: 'foo', b: 'bar' }
+    it('should return correctly object redacted', () => {
+      const fieldsToRedact = [{ field: 'a' }]
+      const actual = redact(obj, fieldsToRedact)
+
+      const expected = { a: '[REDACT]', b: 'bar' }
+      expect(actual).toStrictEqual(expected)
+    })
+
+    it('should return not redacted object as fields in config are not present', () => {
+      const fieldsToRedact = [{ field: 'c' }]
+      const actual = redact(obj, fieldsToRedact)
+
+      const expected = { a: 'foo', b: 'bar' }
+      expect(actual).toStrictEqual(expected)
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import { ConfigRedact, SingleConfig } from './example/simple'
-
-const isObject = (data: any): Boolean => {
-  return typeof data === 'object' && !Array.isArray(data) && data !== null
-}
+import { isObject } from './lib/utils'
+import { ConfigRedact, SingleConfig } from './typings/ConfigRedact'
 
 export const redact = (obj: Record<string, any>, toRedact: ConfigRedact) => {
   const result = JSON.parse(JSON.stringify(obj))
 
   const internalRedact = (
     obj: Record<string, any>,
-    toRedact: any,
+    toRedact: ConfigRedact,
     result: Record<string, any>
   ) => {
     const availableKeys = Object.keys(result)
-    toRedact.forEach((bo: SingleConfig) => {
-      const fieldToManipulate = bo.field
+    toRedact.forEach((fieldToRedact: SingleConfig) => {
+      const fieldToManipulate = fieldToRedact.field
       if (availableKeys.includes(fieldToManipulate)) {
-        if (isObject(result[bo.field]) && bo.data) {
-          internalRedact(obj[bo.field], bo.data, result[bo.field])
+        if (isObject(result[fieldToRedact.field]) && fieldToRedact.data) {
+          internalRedact(
+            obj[fieldToRedact.field],
+            fieldToRedact.data,
+            result[fieldToRedact.field]
+          )
           return
         }
-        return (result[bo.field] = '[REDACT]')
+        return (result[fieldToRedact.field] = '[REDACT]')
       }
     })
     return
