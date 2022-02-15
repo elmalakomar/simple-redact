49e4cbeb5e1c209ca622310d54cd4aa9d7236dd6
omar.elmalak <omar.elmalak@mia-platform.eu>
2022-02-15 08:28:13 +0100

    implemented function that builds configuration from list of strings

--- src/lib/buildConfig.ts
+++ src/lib/buildConfig.ts
@@ -1,18 +1,38 @@
 
-import { ConfigRedact } from '../typings/ConfigRedact'
+import { ConfigRedact, SingleConfig } from '../typings/ConfigRedact'
 
+const separator = '.'
 export const buildConfig = (config: string[]) => {
   const result: ConfigRedact = []
   config.forEach(field => {
-    const fieldConfig = result.find(simpleConfig => simpleConfig.field === field)
-    if(!fieldConfig) {
-      result.push({ field })
+    let splittedField = [field]
+    if(field.includes(separator)) {
+      splittedField = field.split(separator)
     }
+    buildFieldConfig(splittedField, result)
   });
   return result
 }
 
-const strings = ['a', 'c','a']
+const buildFieldConfig = (splittedField: string[], result: ConfigRedact, index = 0) => {
+  if(index >= splittedField.length) {
+    return
+  }
+  let simpleFieldConfig = result.find(x => x.field === splittedField[index])
+  if(!simpleFieldConfig) {
+    const configToAdd: SingleConfig = {
+      field: splittedField[index],
+      data: [],
+    }
+    const configIndex = result.push(configToAdd) - 1
+    buildFieldConfig(splittedField, result[configIndex].data as ConfigRedact, index + 1)
+    return
+  }
+  buildFieldConfig(splittedField, simpleFieldConfig.data as ConfigRedact, index +1)
+  return
+}
+
+const strings = ['a', 'c','a.b.x']
 const config = buildConfig(strings)
-console.log(config)
+console.log(JSON.stringify(config))
 
